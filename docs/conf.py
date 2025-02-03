c7e74ae394ae6183c680bee17fbf21352ecfa88c
Hart Woolery <gohart@gmail.com>
2025-02-03 14:57:58 -0800

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from sphinx.ext import autodoc, viewcode
+
+# Add the project directory to the sys.path
+sys.path.insert(0, os.path.abspath('../..'))
+
+# Project information
+project = 'fx_api'
+author = 'SlapFX'
+release = '0.1.0'
+
+# General configuration
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',  # For Google style docstrings
+    'sphinx.ext.intersphinx',  # For linking to other projects
+]
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+}
+
+# Paths that contain templates, relative to this directory
+templates_path = ['_templates']
+
+# List of patterns to ignore when looking for source files
+exclude_patterns = []
+
+# The master toctree document
+master_doc = 'index'
+
+# HTML output options
+html_theme = 'alabaster'
+html_static_path = ['_static']
